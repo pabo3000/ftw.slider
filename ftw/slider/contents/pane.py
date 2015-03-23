a4af99644d06e7c7d404b27f628e8f438c864be1
Pavel Bogdanovic <pb@riboli.eu>
2015-03-23 14:21:37 +0100

    Adjust slider to fs.

--- ftw/slider/contents/pane.py
+++ ftw/slider/contents/pane.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from ftw.slider import _
 from ftw.slider.interfaces import IPane
 from plone.app.textfield import RichText
@@ -10,13 +11,16 @@ from plone.namedfile.field import NamedImage
 from zope import schema
 from zope.interface import implements
 
+from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
+
+
+alignments = SimpleVocabulary(
+    [SimpleTerm(value=u'left', title=_(u'links oben')),
+     SimpleTerm(value=u'right', title=_(u'rechts unten')), ]
+    )
+
 
 class IPaneSchema(Schema):
-    text = RichText(
-        title=_(u'label_text', default=u'Text'),
-        description=_(u'help_text', default=u''),
-        required=False,
-        )
     image = NamedImage(
         title=_(u'label_image', default='Image'),
         required=True,
@@ -27,6 +31,14 @@ class IPaneSchema(Schema):
         description=_(u'help_link', default=u''),
         required=False,
         source=PathSourceBinder()
+        )
+    align = schema.Choice(
+        title=_(u'label_align', default=u'Ausrichtung'),
+        description=_(u'help_align', default=u"""Der Text lässt sich links oben
+        oder rechts unten ausrichten."""),
+        required=False,
+        vocabulary=alignments,
+        default=u'right',
         )
 
 
